2c6f74bafa3d8c16c3f0c95f81f8bbb2aaad9152
mikkimarko <margowitch123@gmail.com>
2026-02-19 14:59:12 +0200

    Final commit HW2

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,45 +1,26 @@
 package org.skypro.skyshop;
 
-
 import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.Product;
-
+import org.skypro.skyshop.product.SimpleProduct;
 
 public class App {
     public static void main(String[] args) {
 
-        Product bread = new Product("Хлеб", 50);
-        Product milk = new Product("Молоко", 80);
-        Product cheese = new Product("Сыр", 200);
-        Product apple = new Product("Яблоко", 30);
-        Product tea = new Product("Чай", 120);
-        Product coffee = new Product("Кофе", 300);
+        Product bread = new SimpleProduct("Хлеб", 50);
+        Product milk = new DiscountedProduct("Молоко", 80, 20);
+        Product cheese = new FixPriceProduct("Сыр");
+        Product tea = new DiscountedProduct("Чай", 120, 10);
 
         ProductBasket basket = new ProductBasket();
 
         basket.addProduct(bread);
         basket.addProduct(milk);
         basket.addProduct(cheese);
-        basket.addProduct(apple);
         basket.addProduct(tea);
 
-        basket.addProduct(coffee);
-
         basket.printBasket();
-
-        System.out.println("Общая стоимость: " + basket.totalPrice());
-
-        System.out.println("Есть ли Хлеб? " + basket.containsProduct("Хлеб"));
-
-        System.out.println("Есть ли Кофе? " + basket.containsProduct("Кофе"));
-
-        basket.clear();
-
-        basket.printBasket();
-
-        System.out.println("Стоимость пустой корзины: " + basket.totalPrice());
-
-        System.out.println("Есть ли Хлеб в пустой корзине? "
-                + basket.containsProduct("Хлеб"));
     }
 }
