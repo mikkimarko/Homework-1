7cbd1bb4b42644da49e8a6bc225417e1cf83ffec
mikkimarko <margowitch123@gmail.com>
2026-02-23 17:00:57 +0200

    Final commit

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,10 +1,13 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.article.Article;
 import org.skypro.skyshop.basket.ProductBasket;
 import org.skypro.skyshop.product.DiscountedProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.product.SimpleProduct;
+import org.skypro.skyshop.search.SearchEngine;
+import org.skypro.skyshop.search.Searchable;
 
 public class App {
     public static void main(String[] args) {
@@ -22,5 +25,38 @@ public class App {
         basket.addProduct(tea);
 
         basket.printBasket();
+
+        SearchEngine searchEngine = new SearchEngine(10);
+
+        searchEngine.add(bread);
+        searchEngine.add(milk);
+        searchEngine.add(cheese);
+        searchEngine.add(tea);
+
+        Article article1 = new Article("Как выбрать чай", "Чай бывает черный, зеленый и травяной");
+        Article article2 = new Article("Все о молоке", "Молоко бывает коровье и растительное");
+
+        searchEngine.add(article1);
+        searchEngine.add(article2);
+
+
+        System.out.println("\nПоиск: чай");
+        printSearchResults(searchEngine.search("Чай"));
+
+        System.out.println("\nПоиск: молоко");
+        printSearchResults(searchEngine.search("Молоко"));
+
+        System.out.println("\nПоиск: сыр");
+        printSearchResults(searchEngine.search("Сыр"));
+    }
+
+    private static void printSearchResults(Searchable[] results) {
+        for (Searchable item : results) {
+            if (item != null) {
+                System.out.println(item.getStringRepresentation());
+            }
+        }
     }
 }
+
+
